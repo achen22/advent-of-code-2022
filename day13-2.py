a45f244b6fb323fa209c07ca575c754c08d736f5
Alan <alanchen.2003@gmail.com>
2022-12-13 18:08:25 +1100

    Replace eval() with list parser

--- day13-2.py
+++ day13-2.py
@@ -1,7 +1,26 @@
 class NestedIntList(list):
     def parse(s):
         if type(s) == str:
-            s = eval(s)
+            assert s[0] == '[' and s[-1] == ']'
+            #expected = eval(s)
+            tokens = []
+            for c in s[1:-1]:
+                if tokens and c.isdigit() and tokens[-1].isdigit():
+                    tokens[-1] += c
+                else:
+                    tokens.append(c)
+            tree = [[]]
+            for t in tokens:
+                if t.isdigit():
+                    tree[-1].append(int(t))
+                elif t == '[':
+                    s = []
+                    tree[-1].append(s)
+                    tree.append(s)
+                elif t == ']':
+                    tree.pop()
+            s = tree[0]
+            #assert s == expected
         result = s[:]
         for i in range(len(s)):
             if type(s[i]) != int:
