e56b9ac074f49aa4a10a6eee4ea5aac9a9fab629
Alan <alanchen.2003@gmail.com>
2022-12-24 17:53:37 +1100

    Day 24

---
+++ day24-2.py
@@ -0,0 +1,100 @@
+def display(valley, width):
+    for i in range(len(valley)):
+        if i % width == 0:
+            print()
+        tile = valley[i]
+        if len(tile) == 0:
+            print('.', end="")
+        elif len(tile) == 1:
+            print(tile[0], end="")
+        else:
+            print(len(tile), end="")
+    print()
+
+def move(valley, width, i):
+    moves = [i]
+    if i % width != 0:
+        moves.append(i - 1)
+    if i % width != width - 1:
+        moves.append(i + 1)
+    if i >= width:
+        moves.append(i - width)
+    if i < len(valley) - width:
+        moves.append(i + width)
+    return moves
+
+def turn(valley, width, spawn = 0):
+    step = {'^': [], 'v': [], '<': [], '>': [], 'E': []}
+    for i in range(len(valley)):
+        tile = valley[i]
+        for c in tile:
+            if c == 'E':
+                step['E'].extend(move(valley, width, i))
+            else:
+                step[c].append(blizzard[c](i))
+        tile.clear()
+    for c in blizzard.keys():
+        for i in step[c]:
+            valley[i].append(c)
+    step = set(step['E'])
+    for i in step:
+        if len(valley[i]) == 0:
+            valley[i].append('E')
+    if len(valley[spawn]) == 0:
+        valley[spawn].append('E')
+    #display(valley, width)
+
+with open("input") as f:
+    line = f.readline()
+    assert line[:3] == "#.#"
+    width = len(line.rstrip()) - 2
+    valley = []
+    for line in f.readlines():
+        assert line[0] == '#'
+        if line[1] == '#':
+            break
+        for c in line[1:]:
+            if c == '#':
+                break
+            valley.append([] if c == '.' else [c])
+    end = len(valley) - width + line.index('.') - 1
+
+blizzard = {
+    '^': lambda x: (x - width) % len(valley),
+    'v': lambda x: (x + width) % len(valley),
+    '<': lambda x: x // width * width + (x - 1) % width,
+    '>': lambda x: x // width * width + (x + 1) % width
+}
+
+#display(valley, width)
+count = 0
+while valley[end] != ['E']:
+    turn(valley, width)
+    count += 1
+
+#display(valley, width)
+count += 1
+#print(count)
+turn(valley, width)
+for tile in valley:
+    if tile == ['E']:
+        tile.clear()
+
+while valley[0] != ['E']:
+    turn(valley, width, end)
+    count += 1
+
+#display(valley, width)
+count += 1
+#print(count)
+turn(valley, width)
+for tile in valley:
+    if tile == ['E']:
+        tile.clear()
+
+while valley[end] != ['E']:
+    turn(valley, width)
+    count += 1
+
+#display(valley, width)
+print(count + 1)
